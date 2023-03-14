6802a28966245eff84222be04217621e1d018e5e
The Stig <TheStig294@users.noreply.github.com>
2023-03-14 20:40:50 +1030

    Fixed boomerang and pedguin trophies not unlocking

--- lua/ttt_trophies/trophies/boomerang.lua
+++ lua/ttt_trophies/trophies/boomerang.lua
@@ -1,48 +1,36 @@
 local TROPHY = {}
 TROPHY.id = "boomerang"
-TROPHY.title = "It's a 1-shot if it hits you twice!"
-TROPHY.desc = "Kill Zylus in 1 shot of a boomerang"
+TROPHY.title = "1-shot if it hits you twice!"
+TROPHY.desc = "Kill Zylus in 1 shot of a boomerang, or as Zylus, be killed in one shot"
 TROPHY.rarity = 3
 
--- Trophy trigger function only runs when the boomerang randomat is triggered
-hook.Add("TTTRandomatTriggered", "TTTTrophiesBoomerang", function(id)
-    if id == "boomerang" then
-        TROPHY:Trigger(true)
-    end
-end)
-
-function TROPHY:Trigger(boomerangTriggered)
+function TROPHY:Trigger()
     local boomerangEnt
 
     self:AddHook("PostEntityTakeDamage", function(ent, dmg, took)
-        if not boomerangTriggered then return end
-        if not took or not IsValid(ent) or not ent:IsPlayer() then return end
-        if ent:GetModel() ~= "models/player/jenssons/kermit.mdl" then return end
         local inflictor = dmg:GetInflictor()
+        if not took or not IsValid(inflictor) or inflictor:GetClass() ~= "ent_boomerangclose_randomat" then return end
+        if not IsValid(ent) or not ent:IsPlayer() then return end
+        if ent:GetModel() ~= "models/player/jenssons/kermit.mdl" then return end
         local attacker = dmg:GetAttacker()
-        if not IsValid(inflictor) or not IsValid(attacker) or not attacker:IsPlayer() then return end
-
+        if not IsValid(attacker) or not attacker:IsPlayer() then return end
         -- Only keep track of the last boomerang that hit Zylus
-        if inflictor:GetClass() == "ent_boomerangclose_randomat" then
-            boomerangEnt = inflictor
-        end
+        boomerangEnt = inflictor
     end)
 
     self:AddHook("DoPlayerDeath", function(ply, attacker, dmg)
-        if not boomerangTriggered then return end
         if ply:GetModel() ~= "models/player/jenssons/kermit.mdl" then return end
         local inflictor = dmg:GetInflictor()
         if not IsValid(inflictor) or not IsValid(attacker) or not attacker:IsPlayer() then return end
 
         -- If the same boomerang that originally damaged Zylus kills him, the attacker earns the trophy
         if inflictor:GetClass() == "ent_boomerangclose_randomat" and IsValid(boomerangEnt) and inflictor == boomerangEnt then
-            self:Earn(attacker)
+            self:Earn({attacker, ply})
         end
     end)
 
     self:AddHook("TTTEndRound", function()
         boomerangEnt = nil
-        boomerangTriggered = false
     end)
 end
 
--- lua/ttt_trophies/trophies/pedguin.lua
+++ lua/ttt_trophies/trophies/pedguin.lua
@@ -1,23 +1,22 @@
 local TROPHY = {}
 TROPHY.id = "pedguin"
 TROPHY.title = "Barrel for Ped"
-TROPHY.desc = "Prop-kill Pedguin while he isn't on your team"
+TROPHY.desc = "Prop-kill Pedguin, or as Pedguin, get prop-killed"
 TROPHY.rarity = 3
 
 function TROPHY:Trigger()
     self:AddHook("DoPlayerDeath", function(ply, attacker, dmg)
         local infilctor = dmg:GetInflictor()
         if not IsPlayer(attacker) or not IsValid(infilctor) then return end
-        if attacker.IsSameTeam and attacker:IsSameTeam(ply) then return end
 
-        if string.Left(infilctor:GetClass(), 12) == "prop_physics" and ply:GetModel() == "models/kaesar/buffwinniethepooh/buffwinniethepooh.mdl" then
-            self:Earn(attacker)
+        if string.Left(infilctor:GetClass(), 12) == "prop_physics" and (ply:GetModel() == "models/kaesar/buffwinniethepooh/buffwinniethepooh.mdl" or ply:GetModel() == "models/kaesar/buffwinniethepooh/buffwinniethepooh2.mdl") then
+            self:Earn({attacker, ply})
         end
     end)
 end
 
 function TROPHY:Condition()
-    return CR_VERSION and util.IsValidModel("models/kaesar/buffwinniethepooh/buffwinniethepooh.mdl")
+    return util.IsValidModel("models/kaesar/buffwinniethepooh/buffwinniethepooh.mdl")
 end
 
 RegisterTTTTrophy(TROPHY)
