a5bece1e7250ca6406cc0150f6e1520d43a43dac
The Stig <TheStig294@users.noreply.github.com>
2023-04-24 20:44:40 +0930

    Fixed "BAM!" trophy not being able to be earned

--- lua/ttt_trophies/trophies/tom.lua
+++ lua/ttt_trophies/trophies/tom.lua
@@ -3,27 +3,23 @@ TROPHY.id = "tom"
 TROPHY.title = "BAM!"
 TROPHY.desc = "Through the \"Somehow, Palpatine returned!\" randomat, see Tom-bot win"
 TROPHY.rarity = 3
+local palpRandomat = false
 
-function TROPHY:Trigger()
-    local palpRandomat = false
-
-    self:AddHook("TTTRandomatTriggered", function(id, owner)
+if SERVER then
+    hook.Add("TTTRandomatTriggered", "TTTTrophiesBamRandomatTrigger", function(id, owner)
         if id == "palp" then
             palpRandomat = true
         end
     end)
+end
 
+function TROPHY:Trigger()
     self:AddHook("TTTEndRound", function()
         if not palpRandomat then return end
         local tom = GetGlobalEntity("RandomatTomBot")
-        if not IsValid(tom) then return end
 
-        for _, bot in ipairs(player.GetBots()) do
-            if bot == tom and self:IsAlive(bot) then
-                self:Earn(player.GetAll())
-
-                return
-            end
+        if IsValid(tom) and self:IsAlive(tom) then
+            self:Earn(player.GetAll())
         end
 
         palpRandomat = false
